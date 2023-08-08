52de6bfa500c6ac980c1951240335006cff7cab0
Carmine Di Gruttola <carmine.di.gruttola@pwc.com>
2023-08-08 17:04:32 +0200

    some fix

--- _dev/js/checkout/checkout-payment.js
+++ _dev/js/checkout/checkout-payment.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line camelcase
 const ps_checkout_paypal = $('[data-module-name^="ps_checkout-paypal"]');
 // eslint-disable-next-line camelcase
-if (ps_checkout_paypal !== null) {
+if (ps_checkout_paypal !== null && ps_checkout_paypal.prop('checked')) {
   // eslint-disable-next-line camelcase
   ps_checkout_paypal.one('click', () => {
     // eslint-disable-next-line camelcase
